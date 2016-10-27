fd27b63767803e281270d16b1f899b089da5ad00
John <hello@johnmyrda.com>
2016-10-26 20:21:30 -0700

    Fixed compile error, altered "Alternating Colors" pattern, and reformatted code

--- suspenders.ino
+++ suspenders.ino
@@ -14,19 +14,20 @@
 #define DATA_PIN01 11
 #define DATA_PIN02 12
 
-#define NUM_PATTERNS 3
+#define NUM_PATTERNS 4
 typedef void (* GenericFP)();
-GenericFP patterns[NUM_PATTERNS] = {&usa, &rainbow, 
-	&chasing_random_colors_alternating, &chasing_random_colors};
+GenericFP patterns[NUM_PATTERNS] = {&usa, &rainbow,
+                                    &chasing_random_colors_alternating, &chasing_random_colors
+                                   };
 #define NUM_LINES 2
 
-	unsigned long frame = 0;
-  unsigned long pattern_start_ts = 0;
-	unsigned long pattern_duration = 0;
-	int current_pattern_index = 0;
-	int frame_interval = 150;
+unsigned long frame = 0;
+unsigned long pattern_start_ts = 0;
+unsigned long pattern_duration = 0;
+int current_pattern_index = 0;
+int frame_interval = 150;
 
-	CRGB leds[NUM_LINES][NUM_LEDS];
+CRGB leds[NUM_LINES][NUM_LEDS];
 
 // Defines for p_color_temp
 #define TEMPERATURE_1 Tungsten100W
@@ -37,100 +38,100 @@ GenericFP patterns[NUM_PATTERNS] = {&usa, &rainbow,
 #define BLACKTIME   3
 
 
-	void setup() { 
-		FastLED.addLeds<WS2812B, DATA_PIN01, GRB>(leds[0], NUM_LEDS);
-		FastLED.addLeds<WS2812B, DATA_PIN02, GRB>(leds[1], NUM_LEDS);
-		FastLED.clear();
-		FastLED.setBrightness(BRIGHTNESS_DEFAULT);
-		FastLED.show();
-	}
+void setup() {
+  FastLED.addLeds<WS2812B, DATA_PIN01, GRB>(leds[0], NUM_LEDS);
+  FastLED.addLeds<WS2812B, DATA_PIN02, GRB>(leds[1], NUM_LEDS);
+  FastLED.clear();
+  FastLED.setBrightness(BRIGHTNESS_DEFAULT);
+  FastLED.show();
+}
 
-	void loop() {
-		unsigned long now = millis();
-    unsigned long elapsed = now - pattern_start_ts;
-    //check if the pattern time has elapsed - overflow safe!
-		if(elapsed > pattern_duration){
-      pattern_duration = 4000;//random16(4000,16000)
-			pattern_start_ts = now;
-			current_pattern_index = (current_pattern_index + 1) % NUM_PATTERNS;
-		}
-		unsigned long newFrame = now/frame_interval;
-		if(newFrame != frame){
-			frame = newFrame;
-			patterns[current_pattern_index]();
-			FastLED.show();			
-		}
+void loop() {
+  unsigned long now = millis();
+  unsigned long elapsed = now - pattern_start_ts;
+  //check if the pattern time has elapsed - overflow safe!
+  if (elapsed > pattern_duration) {
+    pattern_duration = 10000;//random16(4000,16000)
+    pattern_start_ts = now;
+    current_pattern_index = (current_pattern_index + 1) % NUM_PATTERNS;
+  }
+  unsigned long newFrame = now / frame_interval;
+  if (newFrame != frame) {
+    frame = newFrame;
+    patterns[current_pattern_index]();
+    FastLED.show();
+  }
 
-	}
+}
 
 
-	CRGB get_random_color() {
-		CHSV hsv(random8(), 255, 255);
-		CRGB rgb;
-		hsv2rgb_rainbow( hsv, rgb);
-		return rgb;
-	}
+CRGB get_random_color() {
+  CHSV hsv(random8(), 255, 255);
+  CRGB rgb;
+  hsv2rgb_rainbow( hsv, rgb);
+  return rgb;
+}
 
-	void rainbow() {
-		int rainbow_density = 8; //lower==less dense
-		increment_all_leds();
-		for(int z=0;z<NUM_LINES;z++){
-			fill_rainbow(leds[z], 1, frame*rainbow_density);
-		}
-	}
+void rainbow() {
+  int rainbow_density = 8; //lower==less dense
+  shift_all_leds(1);
+  for (int z = 0; z < NUM_LINES; z++) {
+    fill_rainbow(leds[z], 1, frame * rainbow_density);
+  }
+}
 
-	void usa(){
-		CRGB usa_pattern[9] = {
-            CRGB::Red, CRGB::Red, CRGB::Red, 
-            CRGB::White, CRGB::White, CRGB::White, 
-            CRGB::Blue, CRGB::Blue, CRGB::Blue};
-        generic_pattern(usa_pattern, 9);
-	}
+void usa() {
+  CRGB usa_pattern[9] = {
+    CRGB::Red, CRGB::Red, CRGB::Red,
+    CRGB::White, CRGB::White, CRGB::White,
+    CRGB::Blue, CRGB::Blue, CRGB::Blue
+  };
+  generic_pattern(usa_pattern, 9);
+}
 
-    void generic_pattern(CRGB * pattern, int pattern_length){
-        increment_all_leds();
-        int pattern_index = frame % pattern_length;
-        for(int i = 0; i < NUM_LINES; i++){
-            leds[i][0] = pattern[pattern_index];
-        }
+void generic_pattern(CRGB * pattern, int pattern_length) {
+  shift_all_leds(1);
+  int pattern_index = frame % pattern_length;
+  for (int i = 0; i < NUM_LINES; i++) {
+    leds[i][0] = pattern[pattern_index];
+  }
+}
+
+void shift_leds(CRGB * leds_to_shift, int length, int shift_amount) {
+  CRGB temp[length];
+  for (int x = 0; x < length - shift_amount; x++) {
+    temp[x + shift_amount] = leds_to_shift[x];
+  }
+  memcpy(leds_to_shift, temp, sizeof(CRGB) * length);
+}
+
+void shift_all_leds(int shift_amount) {
+  for (int i = 0; i < NUM_LINES; i++) {
+    shift_leds(leds[i], NUM_LEDS, shift_amount);
+  }
+}
+
+void chasing_random_colors() {
+  chasing_random_colors(false);
+}
+
+void chasing_random_colors_alternating() {
+  chasing_random_colors(true);
+}
+
+void chasing_random_colors(bool alternating) {
+  CRGB new_color = CRGB::Black;
+  if (!leds[0][0] && !leds[0][1]) {
+    new_color = get_random_color();
+  }
+
+  for (int i = 0; i < NUM_LINES; i++) {
+    if (alternating and (i % 2 == 1)) {
+      shift_leds(leds[i], NUM_LEDS, -1);
+      leds[i][NUM_LEDS - 1] = new_color; 
+    } else {
+      shift_leds(leds[i], NUM_LEDS, 1);
+      leds[i][0] = new_color;
     }
-
-	void shift_leds(CRGB * leds_to_shift, int length, int shift_amount){
-		CRGB temp[length];
-		for (int x=0; x < length-shift_amount; x++){
-			temp[x+shift_amount] = leds_to_shift[x];
-		}
-		memcpy(leds_to_shift, temp, sizeof(CRGB) * length);
-	}
-
-	void increment_all_leds(){
-		for(int i = 0; i < NUM_LINES; i++){
-			shift_leds(leds[i], NUM_LEDS, 1);
-		}	
-	}
-
-    void chasing_random_colors(){
-        chasing_random_colors(false);
-    }
-
-    void chasing_random_colors_alternating(){
-        chasing_random_colors(true);
-    }
-
-	void chasing_random_colors(bool alternating) {
-		CRGB new_color = CRGB::Black;	
-		if(!leds[0][0] && !leds[0][1]){
-			new_color = get_random_color();
-		}
-
-	    shift_leds(leds[0], NUM_LEDS, 1);
-        leds[0][0] = new_color;
-
-        for (int i = 0; i < NUM_LEDS; i++){ 
-            if (alternating){
-                leds[1][i] = leds[0][NUM_LEDS - i];
-            } else {
-                leds[1][i] = leds[0][i];
-            }
-        }
-	}
+  }
+}
